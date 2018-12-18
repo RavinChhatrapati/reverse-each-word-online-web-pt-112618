0ac432f55942f4644aa3e719ed80071c44be6ddb
Ravin Chhatrapati <ravinchhatrapati95@gmail.com>
2018-12-18 22:16:23 +0000

    Automatically backed up by Learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,8 @@
+def reverse_each_word(sentence1)
+sentence1 = sentence1.split(" ")
+reversed_string = []
+sentence1.each do |char|
+reversed_string.unshift(char)
+end
+sentence1.split(" "). reverse.join(" ")
+end
