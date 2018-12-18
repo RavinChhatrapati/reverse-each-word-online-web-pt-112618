064e100b7d8557c05fdde7dfc765c2c2243af362
Ravin Chhatrapati <ravinchhatrapati95@gmail.com>
2018-12-18 22:22:33 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,10 @@
+require "pry"
 def reverse_each_word(sentence1)
 sentence1 = sentence1.split
-reversed_string = []
+reversed_array = []
+binding.pry
 sentence1.each do |char|
+
 reversed_string.unshift(char)
 end
 sentence1.split. reverse.join(" ")
