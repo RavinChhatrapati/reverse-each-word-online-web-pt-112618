2ffb1d269c8844be05b89f6e61db5c8d6853e294
Ravin Chhatrapati <ravinchhatrapati95@gmail.com>
2018-12-18 22:34:49 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,7 +3,7 @@ def reverse_each_word(sentence1)
 sentencearray = sentence1.split
 reversed_array = []
 sentencearray.each do |word|
-reversed_array <<  word.reverse 
+reversed_array <<  word.reverse
 end
 binding.pry
 end
